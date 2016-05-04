044aa8cdd3ad856d6577630741ff821fcc8ae0a1
majunliang <majunliang@dongpi.com>
2016-05-04 11:00:45 +0800

    no message

---
+++ ios_runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_runtime
+//
+//  Created by majunliang on 16/5/4.
+//  Copyright © 2016年 majunliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
